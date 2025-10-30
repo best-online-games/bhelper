1bee59a37e719c0b450ea6213e197098b6b53883
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-30 17:47:56 +0300

    f

--- app/prof/prof.view.ts
+++ app/prof/prof.view.ts
@@ -1,20 +1,3 @@
 namespace $.$$ {
-    export class $bog_prof_app_prof extends $.$bog_prof_app_prof {
-        @$mol_mem
-        professions_by_category() {
-            return {
-                ИТ: ['Разработчик', 'Тестировщик', 'Аналитик', 'DevOps'],
-                Дизайн: ['UI/UX дизайнер', 'Графический дизайнер'],
-                Маркетинг: ['Маркетолог', 'SMM-менеджер'],
-            } as const
-        }
-
-        @$mol_mem
-        prof_rows(): readonly any[] {
-            const ids = this.professions_by_category()
-            console.log(ids)
-            // return ids.map(id => this.Row(id))
-			return ['Разработчик', 'Тестировщик', 'Аналитик', 'DevOps']
-        }
-    }
+  export class $bog_prof_app_prof extends $.$bog_prof_app_prof {}
 }
